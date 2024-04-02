29306a93b4c6995292e08d379cd624f5ad637248
Heron Lancellot <heronlancellot@id.uff.br>
2024-04-02 19:30:48 -0300

    fix: removing comment line

--- lib/service/verifyTokenOwnership.ts
+++ lib/service/verifyTokenOwnership.ts
@@ -48,32 +48,6 @@ export async function verifyTokenOwnership({
         };
       } else throw new Error("Invalid Token balance response type");
     }
-
-    // onWalletConfirmation();
-    // let txReceipt = {} as TransactionReceipt;
-    // while (typeof txReceipt.blockHash === "undefined") {
-    /*
-          It is guaranteed that at some point we'll have a valid TransactionReceipt in here.
-          If we had a valid transaction sent (which is confirmed at this point by the try/catch block),
-          it is a matter of waiting the transaction to be mined in order to know whether it was successful or not.
-          So why are we using a while loop here?
-          - Because it is possible that the transaction was not yet mined by the time
-          we reach this point. So we keep waiting until we have a valid TransactionReceipt.
-        */
-    //   const transactionReceipt = await publicClient({
-    //     chainId,
-    //   }).waitForTransactionReceipt({
-    //     hash: transactionHash,
-    //   });
-    //   if (transactionReceipt) {
-    //     txReceipt = transactionReceipt;
-    //   }
-    // }
-    // return {
-    //   success: true,
-    //   receipt: txReceipt,
-    //   errorMessage: null,
-    // };
   } catch (error) {
     console.error(error);
     toast.error(
