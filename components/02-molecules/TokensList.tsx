334cd3af7675808b7e2be98e7b0f864cda1917b4
Heron Lancellot <heronlancellot@id.uff.br>
2024-04-04 18:02:12 -0300

    fix: slot offer summary

--- components/02-molecules/TokensList.tsx
+++ components/02-molecules/TokensList.tsx
@@ -95,7 +95,7 @@ export const TokensList = ({
         totalCardsLength:
           withAddTokenCard && variant === ForWhom.Your
             ? tokensList.length + 1 // Removes one empty square, so there is space for addTokenSquare
-            : tokensList.length + 1, // Removes one empty square, so there is space for addTokenSquare
+            : tokensList.length,
         mobileTotalSquares: mobileTotalCards,
         tabletTotalSquares: tabletTotalCards,
         desktopTotalSquares: desktopTotalCards,
