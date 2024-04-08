128e5c95881c0c414b86cda8ed95a8a635d39c50
Rafael Sanches <rafaeldss12@gmail.com>
2024-04-08 14:29:52 -0300

    fix: Added offer-queries file which have all the queries needed for the filters

---
+++ lib/client/offer-queries copy.ts
@@ -0,0 +1,179 @@
+export const ALL_OFFERS_QUERY = `
+         query databases($orderBy: String!, $orderDirection: String!, $inputAddress: String, $after: String, $allowed: String) {
+           databases(
+             orderBy: $orderBy,
+             orderDirection: $orderDirection,
+             where: { OR: [{owner: $inputAddress}, {allowed: $allowed}] },
+             limit: 20,
+             after: $after
+           ) {
+             items {
+               swapId
+               status
+               owner
+               allowed
+               expiry
+               bid
+               ask
+               blockTimestamp
+               transactionHash
+             }
+             pageInfo {
+               hasNextPage
+               endCursor
+             }
+           }
+         }
+       `;
+
+export const CREATED_OFFERS_QUERY = `
+         query databases($orderBy: String!, $orderDirection: String!, $inputAddress: String, $after: String, $expiry_gt: BigInt) {
+           databases(
+             orderBy: $orderBy,
+             orderDirection: $orderDirection,
+             where: { owner: $inputAddress, status: CREATED, expiry_gt: $expiry_gt },
+             limit: 20,
+             after: $after
+           ) {
+             items {
+               swapId
+               status
+               owner
+               allowed
+               expiry
+               bid
+               ask
+               blockTimestamp
+               transactionHash
+             }
+             pageInfo {
+               hasNextPage
+               endCursor
+             }
+           }
+         }
+       `;
+
+export const RECEIVED_OFFERS_QUERY = `
+         query databases($orderBy: String!, $orderDirection: String!, $after: String, $allowed: String, $expiry_gt: BigInt) {
+           databases(
+             orderBy: $orderBy,
+             orderDirection: $orderDirection,
+             where: { allowed: $allowed, status_not: ACCEPTED, expiry_gt: $expiry_gt },
+             limit: 20,
+             after: $after
+           ) {
+             items {
+               swapId
+               status
+               owner
+               allowed
+               expiry
+               bid
+               ask
+               blockTimestamp
+               transactionHash
+             }
+             pageInfo {
+               hasNextPage
+               endCursor
+             }
+           }
+         }
+       `;
+
+export const ACCEPTED_OFFERS_QUERY = `
+         query databases($orderBy: String!, $orderDirection: String!, $inputAddress: String, $after: String, $allowed: String) {
+           databases(
+             orderBy: $orderBy,
+             orderDirection: $orderDirection,
+             where: { AND: [ {status: ACCEPTED}, {OR: [ {owner: $inputAddress},{allowed: $allowed}]}]},
+             limit: 20,
+             after: $after
+           ) {
+             items {
+               swapId
+               status
+               owner
+               allowed
+               expiry
+               bid
+               ask
+               blockTimestamp
+               transactionHash
+             }
+             pageInfo {
+               hasNextPage
+               endCursor
+             }
+           }
+         }
+       `;
+
+export const CANCELED_OFFERS_QUERY = `
+         query databases($orderBy: String!, $orderDirection: String!, $inputAddress: String, $after: String, $allowed: String) {
+           databases(
+             orderBy: $orderBy,
+             orderDirection: $orderDirection,
+             where: { AND: [ {status: CANCELED}, {OR: [ {owner: $inputAddress}, {allowed: $allowed}]}]},
+             limit: 20,
+             after: $after
+           ) {
+             items {
+               swapId
+               status
+               owner
+               allowed
+               expiry
+               bid
+               ask
+               blockTimestamp
+               transactionHash
+             }
+             pageInfo {
+               hasNextPage
+               endCursor
+             }
+           }
+         }
+       `;
+
+export const EXPIRED_OFFERS_QUERY = `
+         query databases($orderBy: String!, $orderDirection: String!, $inputAddress: String, $after: String, $expiry_lt: BigInt) {
+           databases(
+             orderBy: $orderBy,
+             orderDirection: $orderDirection,
+             where: {
+              AND: [
+                {
+                  OR: [
+                    { owner: $inputAddress },
+                    { allowed: $inputAddress }
+                  ]
+                },
+                { status_not: ACCEPTED },
+                { status_not: CANCELED },
+                { expiry_lt: $expiry_lt }
+              ]
+            },
+             limit: 20,
+             after: $after
+           ) {
+             items {
+               swapId
+               status
+               owner
+               allowed
+               expiry
+               bid
+               ask
+               blockTimestamp
+               transactionHash
+             }
+             pageInfo {
+               hasNextPage
+               endCursor
+             }
+           }
+         }
+       `;
