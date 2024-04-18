b1928a925cba1652560304612ebe7c5914ac0935
eduramme <eduramme@gmail.com>
2024-04-18 02:03:03 -0300

    prevent rerenders on offers page

--- components/01-atoms/OffersContext.tsx
+++ components/01-atoms/OffersContext.tsx
@@ -19,6 +19,7 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 import React, { Dispatch, useEffect, useState } from "react";
 import axios from "axios";
 import { isAddress } from "viem";
+import { useAccount } from "wagmi";
 
 export enum PonderFilter {
   ALL_OFFERS = "All Offers",
@@ -70,6 +71,7 @@ export const OffersContextProvider = ({ children }: any) => {
   const [isLoadingOffersQuery, setIsLoadingOffersQuery] = useState(false);
 
   const { authenticatedUserAddress } = useAuthenticatedUser();
+  const { address, isConnected } = useAccount();
 
   const userAddress = authenticatedUserAddress?.address;
 
@@ -223,11 +225,14 @@ export const OffersContextProvider = ({ children }: any) => {
     queryFn: async ({ pageParam }: { pageParam: string | null }) =>
       await fetchSwaps({ pageParam }),
     initialPageParam: null,
+    refetchOnWindowFocus: false,
+    staleTime: Infinity,
     getNextPageParam: (lastPage) => lastPage?.pageInfo?.endCursor,
     enabled: !!authenticatedUserAddress,
   });
 
   const [hasNextPage, setHasNextPage] = useState(false);
+
   useEffect(() => {
     if (data) {
       setHasNextPage(data.pages[data.pages.length - 1].pageInfo.hasNextPage);
