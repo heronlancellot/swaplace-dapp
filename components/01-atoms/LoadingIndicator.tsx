b8a8283387e7d1f373e8e4d6577cf4f23bd54e90
0xneves <asguilift@hotmail.com>
2024-04-19 07:57:18 +0700

    fix: loading color was to bright

--- components/01-atoms/LoadingIndicator.tsx
+++ components/01-atoms/LoadingIndicator.tsx
@@ -3,6 +3,6 @@ import { HTMLProps } from "react";
 export const LoadingIndicator = (props: HTMLProps<HTMLDivElement>) => (
   <div
     {...props}
-    className="animate-spin rounded-full h-4 w-4 border-t-2 dark:border-white border-black"
+    className="animate-spin rounded-full h-4 w-4 border-t-2 dark:border-[#212322] border-[#212322]"
   />
 );
