d73d51160d691ad4443e2b57dbb59c31e2526381
0xneves <asguilift@hotmail.com>
2024-04-19 10:37:00 +0700

    fix: wrong color scheme for swap offers

--- components/01-atoms/StatusOffers.tsx
+++ components/01-atoms/StatusOffers.tsx
@@ -51,7 +51,7 @@ export const StatusOffers = () => {
         return (
           <button
             className={cc([
-              "h-11 w-full dark:shadow-swap-station border border-solid border-[#353836] flex justify-between items-center px-3 bg-[#F2F2F2] group rounded-[10px] dark:bg-[#212322] mb-3 font-onest leading-5",
+              "h-11 w-full border border-solid border-[#D6D5D5] dark:border-[#353836] flex justify-between items-center px-3 bg-[#F6F6F6] group rounded-[10px] dark:bg-[#212322] mb-3 font-onest leading-5 dark:shadow-swap-station shadow-swap-station-light",
               offerIsActive === index
                 ? "dark:shadow-[0px_0px_8px_1px_#83980026] dark:border-[#505150] dark:bg-[#212322]"
                 : "dark:hover:bg-[#282B29]",
