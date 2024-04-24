2f1b57ee73ebced1cf101eb09dfbe6dc1f266c9f
heronlancellot <heronlancellot@id.uff.br>
2024-04-24 16:44:07 -0300

    feat: swap offers inverted bidder and asker

--- components/03-organisms/SwapOffers.tsx
+++ components/03-organisms/SwapOffers.tsx
@@ -23,7 +23,6 @@ import {
   PopulatedSwapOfferCard,
 } from "@/lib/client/offers-utils";
 import { useAuthenticatedUser } from "@/lib/client/hooks/useAuthenticatedUser";
-import cc from "classcat";
 import { useContext, useEffect, useState } from "react";
 
 /**
@@ -81,7 +80,6 @@ export const SwapOffers = () => {
               swap.askerAssets.tokens,
             );
             const swapStatus = findStatus(swap);
-
             return {
               id: BigInt(swap.id),
               status: swapStatus,
@@ -157,23 +155,19 @@ export const SwapOffers = () => {
   );
 };
 
-interface SwapOfferProps {
-  swap: PopulatedSwapOfferCard;
-}
-
-const SwapOffer = ({ swap }: SwapOfferProps) => {
+const SwapOffer = ({ swap }: { swap: PopulatedSwapOfferCard }) => {
   return (
     <div className="flex flex-col no-scrollbar border border-solid border-[#D6D5D5] dark:border-[#353836] dark:shadow-swap-station shadow-swap-station-light dark:bg-[#212322] font-onest rounded-lg ">
       <div className="flex flex-row border-b mb-auto dark:border-[#353836] relative">
-        <div className={cc(["border-r dark:border-[#353836]"])}>
+        <div className={"border-r dark:border-[#353836]"}>
           <SwapOfferCard
             tokens={swap.askerTokens.tokens}
-            address={swap.askerTokens.address}
+            address={swap.bidderTokens.address} // Should be inversed to display different in the UI
           />
         </div>
         <SwapOfferCard
           tokens={swap.bidderTokens.tokens}
-          address={swap.bidderTokens.address}
+          address={swap.askerTokens.address} // Should be inversed to display different in the UI
         />
         <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 border border-[#70757230] bg-[#f6f6f6] dark:bg-[#212322] rounded-[100px] w-[24px] h-[24px] items-center flex justify-center">
           <SwapIcon />
