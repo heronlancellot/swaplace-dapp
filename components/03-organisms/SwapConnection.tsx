fd90ff2aa4706751aa2192725a37ada1eeff2b63
heronlancellot <heronlancellot@id.uff.br>
2024-04-26 11:38:04 -0300

    refactor: remove divs to better visualization

--- components/03-organisms/SwapConnection.tsx
+++ components/03-organisms/SwapConnection.tsx
@@ -3,15 +3,9 @@ import { SelectChainNetwork } from "@/components/02-molecules";
 
 export const SwapConnection = () => {
   return (
-    <>
-      <div className="lg:max-h-[184px] flex flex-col gap-5">
-        <div>
-          <SearchBar />
-        </div>
-        <div>
-          <SelectChainNetwork />
-        </div>
-      </div>
-    </>
+    <div className="lg:max-h-[184px] flex flex-col gap-5">
+      <SearchBar />
+      <SelectChainNetwork />
+    </div>
   );
 };
