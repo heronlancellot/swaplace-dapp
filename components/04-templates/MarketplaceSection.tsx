0334ff7ddbb1ce94212be368573b714a2dd5709f
Heron Lancellot <heronlancellot@id.uff.br>
2024-05-09 10:38:57 -0300

    feat: add filter variant to render also marketplace and offers page

--- components/04-templates/MarketplaceSection.tsx
+++ components/04-templates/MarketplaceSection.tsx
@@ -1,5 +1,9 @@
 import { MarketplaceSwapOffers } from "@/components/03-organisms";
-import { TheHeader } from "@/components/02-molecules";
+import {
+  FilterOffers,
+  FilterVariant,
+  TheHeader,
+} from "@/components/02-molecules";
 
 export const MarketplaceSection = () => {
   return (
@@ -7,6 +11,9 @@ export const MarketplaceSection = () => {
       <TheHeader />
       <section className="flex items-center  xl:px-[60px] xl:pt-[32px] xl:flex-row flex-col">
         <div className="flex xl:flex-row flex-col h-full xl:w-[1098px] w-[95%] justify-between xl:items-start md:items-center">
+          <div className="flex flex-col">
+            <FilterOffers variant={FilterVariant.MARKETPLACE} />
+          </div>
           <div className="flex xl:flex-col w-full items-center h-full overflow-auto no-scrollbar">
             <MarketplaceSwapOffers />
           </div>
