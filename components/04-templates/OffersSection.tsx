32c8e89403536f7c67111d782c4a065c305326ec
heronlancellot.eth <heronlancellot@id.uff.br>
2024-05-16 13:05:44 -0300

    feat: add Filter Offer
    - add filter offer with variant Marketplace or Offers
    
    Co-Authored-By: Rafael de Souza Sanches <102108966+RafaDSan@users.noreply.github.com>

--- components/04-templates/OffersSection.tsx
+++ components/04-templates/OffersSection.tsx
@@ -1,5 +1,9 @@
 import { SwapOffers } from "../03-organisms/SwapOffers";
-import { FilterOffers, TheHeader } from "@/components/02-molecules";
+import {
+  FilterOffers,
+  FilterVariant,
+  TheHeader,
+} from "@/components/02-molecules";
 
 export const OfferSection = () => {
   return (
@@ -8,7 +12,7 @@ export const OfferSection = () => {
       <section className="flex items-center  xl:px-[60px] xl:pt-[32px] xl:flex-row flex-col">
         <div className="flex xl:flex-row flex-col h-full xl:w-[1098px] w-[95%] justify-between xl:items-start md:items-center">
           <div className="flex flex-col">
-            <FilterOffers />
+            <FilterOffers variant={FilterVariant.OFFERS} />
           </div>
           <div className="flex xl:flex-col w-full items-center h-full overflow-auto no-scrollbar">
             <SwapOffers />
--- components/04-templates/OffersSectionMarketplace.tsx
+++ components/04-templates/OffersSectionMarketplace.tsx
@@ -1,5 +1,9 @@
 import { SwapOffersMarketplace } from "../03-organisms/SwapOffersMarketplace";
-import { FilterOffers, TheHeader } from "@/components/02-molecules";
+import {
+  FilterOffers,
+  FilterVariant,
+  TheHeader,
+} from "@/components/02-molecules";
 
 export const OffersSectionMarketplace = () => {
   return (
@@ -8,7 +12,7 @@ export const OffersSectionMarketplace = () => {
       <section className="flex items-center  xl:px-[60px] xl:pt-[32px] xl:flex-row flex-col">
         <div className="flex xl:flex-row flex-col h-full xl:w-[1098px] w-[95%] justify-between xl:items-start md:items-center">
           <div className="flex flex-col">
-            <FilterOffers />
+            <FilterOffers variant={FilterVariant.MARKETPLACE} />
           </div>
           <div className="flex xl:flex-col w-full items-center h-full overflow-auto no-scrollbar">
             <SwapOffersMarketplace />
