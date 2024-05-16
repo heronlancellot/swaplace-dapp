7a7b2098584f940438086c67801af9e004e61ae8
heronlancellot.eth <heronlancellot@id.uff.br>
2024-05-16 12:12:06 -0300

    Create OffersSectionMarketplace.tsx
    - copy of OffersSection
    
    Co-Authored-By: Rafael de Souza Sanches <102108966+RafaDSan@users.noreply.github.com>

---
+++ components/04-templates/OffersSectionMarketplace.tsx
@@ -0,0 +1,20 @@
+import { SwapOffers } from "../03-organisms/SwapOffers";
+import { FilterOffers, TheHeader } from "@/components/02-molecules";
+
+export const OffersSectionMarketplace = () => {
+  return (
+    <div className="max-w-[1280px] max-h-[720px] w-full flex xl:flex-row flex-col lg:justify-center h-full">
+      <TheHeader />
+      <section className="flex items-center  xl:px-[60px] xl:pt-[32px] xl:flex-row flex-col">
+        <div className="flex xl:flex-row flex-col h-full xl:w-[1098px] w-[95%] justify-between xl:items-start md:items-center">
+          <div className="flex flex-col">
+            <FilterOffers />
+          </div>
+          <div className="flex xl:flex-col w-full items-center h-full overflow-auto no-scrollbar">
+            <SwapOffers />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
