3752442fd088d644ec48858a936fe72abaa1a158
Rafael Sanches <rafaeldss12@gmail.com>
2024-05-27 12:34:29 -0300

    feat: `estimageGas` method working correctly

--- lib/service/acceptSwap.ts
+++ lib/service/acceptSwap.ts
@@ -1,8 +1,7 @@
 import { SWAPLACE_SMART_CONTRACT_ADDRESS } from "../client/constants";
 import { EthereumAddress } from "../shared/types";
 import { publicClient } from "../wallet/wallet-config";
-import { encodeFunctionData, parseEther } from "viem";
-import { ethers } from "ethers";
+import { encodeFunctionData } from "viem";
 
 export interface SwapUserConfiguration {
   walletClient: any;
@@ -44,24 +43,23 @@ export async function acceptSwap(
     args: [swapId, receiver.address],
   });
 
-  const provider = new ethers.providers.JsonRpcProvider(
-    "https://eth-sepolia.g.alchemy.com/v2/D4cZZW64N8gn71Xp8Vk5lr6CNlLMCdqr",
-  );
-
   try {
-    const contractAddress = SWAPLACE_SMART_CONTRACT_ADDRESS[
-      configurations.chain
-    ] as `0x${string}`;
-    const estimatedGas = await provider.estimateGas({
-      to: contractAddress,
+    const gasLimit = await publicClient({
+      chainId: configurations.chain,
+    }).estimateGas({
+      account: configurations.walletClient.account as `0x${string}`,
       data: data,
+      to: SWAPLACE_SMART_CONTRACT_ADDRESS[
+        configurations.chain
+      ] as `0x${string}`,
     });
 
     const transactionHash = await configurations.walletClient.sendTransaction({
-      to: contractAddress,
       data: data,
-      gasLimit: estimatedGas,
-      maxPriorityFeePerGas: parseEther("1"),
+      to: SWAPLACE_SMART_CONTRACT_ADDRESS[
+        configurations.chain
+      ] as `0x${string}`,
+      gasLimit: gasLimit,
     });
 
     const transactionReceipt = await publicClient({
