e9e3ab211e4df84d75ac52dbca71ed9d6108bcdd
heronlancellot.eth <heronlancellot@id.uff.br>
2024-06-01 14:08:08 -0300

    feat: add LeaderboardContext

--- lib/client/contexts/LeaderboardContext.tsx
+++ lib/client/contexts/LeaderboardContext.tsx
@@ -1,121 +1,43 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
-import { SwapContext } from "./SwapContext";
-import { useAuthenticatedUser } from "../hooks/useAuthenticatedUser";
-import { RawLeaderboardDataInterface } from "../ponder-utils";
-import { PageInfo } from "../offers-utils";
-import { ADDRESS_ZERO } from "../constants";
-import { fetchLeaderboard } from "@/lib/service/fetchLeaderboard";
-import { createContext, useContext, useEffect, useState } from "react";
-import { useInfiniteQuery, useNetwork } from "wagmi";
+import { LeaderboardData } from "../leaderboard-utils";
+import {
+  Dispatch,
+  SetStateAction,
+  createContext,
+  useEffect,
+  useState,
+} from "react";
 
 interface LeaderboardContextProps {
-  fetchNextPage: () => void;
-  isFetchingNextPage: boolean;
-  hasNextPage: boolean;
-  // isLoadingOffersQuery: boolean;
-  // isError: boolean;
-  data:
-    | {
-        profileData: RawLeaderboardDataInterface[];
-        pageInfo: PageInfo;
-      }[]
-    | undefined;
+  leaderboardData: LeaderboardData[];
+  setLeaderboardData: Dispatch<SetStateAction<LeaderboardData[]>>;
 }
-
-export interface InfiniteQueryData {
-  pages: {
-    profileData: RawLeaderboardDataInterface[];
-    pageInfo: PageInfo;
-  };
-}
-
-const DEFAULT_PAGE_INFO: PageInfo = {
-  hasNextPage: false,
-  endCursor: null,
-};
-
-const DEFAULT_PROFILE_DATA: RawLeaderboardDataInterface[] = [
-  {
-    id: ADDRESS_ZERO,
-    totalScore: BigInt(1),
-  },
-];
-
-const DEFAULT_DATA_INFINITE_QUERY: InfiniteQueryData = {
-  pages: {
-    profileData: DEFAULT_PROFILE_DATA,
-    pageInfo: DEFAULT_PAGE_INFO,
-  },
-};
 
 export const LeaderboardContextProvider = ({ children }: any) => {
-  const { authenticatedUserAddress } = useAuthenticatedUser();
-  const { destinyChain } = useContext(SwapContext);
-
-  const userAddress = authenticatedUserAddress?.address;
-  const { chain } = useNetwork();
-
-  let chainId: number;
-
-  if (typeof chain?.id !== "undefined") {
-    chainId = chain?.id;
-  }
-
-  // Leaderboard query
-  const { data, isFetchingNextPage, fetchNextPage } = useInfiniteQuery({
-    queryKey: [
-      "PonderQueryLeaderboard",
-      authenticatedUserAddress,
-      destinyChain,
-    ],
-    queryFn: async ({ pageParam }: { pageParam: string | null }) =>
-      await fetchLeaderboard({
-        pageParam: pageParam,
-        userAddress: userAddress,
-        chainId: chainId,
-      }),
-    initialPageParam: null,
-    refetchOnWindowFocus: false,
-    staleTime: Infinity,
-    getNextPageParam: (lastPage) => lastPage?.pageInfo?.endCursor,
-    enabled: !!authenticatedUserAddress,
-  });
-
-  const [hasNextPage, setHasNextPage] = useState(false);
+  const [leaderboardData, setLeaderboardData] = useState<LeaderboardData[]>([]);
 
   useEffect(() => {
-    if (data) {
-      setHasNextPage(data.pages[data.pages.length - 1].pageInfo.hasNextPage);
-    }
-  }, [data]);
-
-  useEffect(() => {
-    setLeaderboardData({
-      fetchNextPage,
-      isFetchingNextPage,
-      data,
-      hasNextPage,
+    setLeaderboardContextData({
+      leaderboardData,
+      setLeaderboardData,
     });
-  }, [data]);
+  }, [leaderboardData]);
 
   // Exportable data
-  const [leaderboardData, setLeaderboardData] =
+  const [leaderboardContextData, setLeaderboardContextData] =
     useState<LeaderboardContextProps>({
-      fetchNextPage,
-      isFetchingNextPage,
-      data,
-      hasNextPage,
+      leaderboardData,
+      setLeaderboardData,
     });
+
   return (
-    <LeaderboardContext.Provider value={leaderboardData}>
+    <LeaderboardContext.Provider value={leaderboardContextData}>
       {children}
     </LeaderboardContext.Provider>
   );
 };
 
 export const LeaderboardContext = createContext<LeaderboardContextProps>({
-  fetchNextPage: () => {},
-  isFetchingNextPage: false,
-  data: [DEFAULT_DATA_INFINITE_QUERY.pages],
-  hasNextPage: false,
+  leaderboardData: [],
+  setLeaderboardData: () => {},
 });
