c02ca7e3989fb293f627bccf46e8a11b8940f7c6
heronlancellot.eth <heronlancellot@id.uff.br>
2024-06-10 17:09:04 -0300

    fix: hydration error mounted component

--- components/02-molecules/EtherFieldAddition.tsx
+++ components/02-molecules/EtherFieldAddition.tsx
@@ -2,22 +2,29 @@ import { EtherAmountSelectionModal } from "@/components/02-molecules";
 import { EditionIcon, PlusIconSmall } from "@/components/01-atoms/";
 import { SwapContext } from "@/lib/client/contexts";
 import { ForWhom } from "@/components/03-organisms";
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { formatEther } from "viem";
 import { useNetwork } from "wagmi";
 
 export const EtherFieldAddition = ({ variant }: { variant: ForWhom }) => {
   const [open, setIsOpen] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
   const { etherValue } = useContext(SwapContext);
   const { chain } = useNetwork();
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
   console.log("variant", variant);
 
   return (
     <div className="flex gap-2">
-      <p className=" flex items-center p-small-variant-black-3 dark:p-small-variant-light-2">
-        {formatEther(etherValue)} {chain?.nativeCurrency.symbol}
+      <p className="flex items-center p-small-variant-black-3 dark:p-small-variant-light-2">
+        {formatEther(etherValue)}
       </p>
+      {/* Only render the chain symbol after the component has mounted */}
+      <p>{isMounted && chain ? chain.nativeCurrency.symbol : ""}</p>
 
       {etherValue === 0n ? (
         <>
