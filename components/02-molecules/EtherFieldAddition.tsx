a186cca5e4d3ecc35f2ee86c6300c911650b0239
heronlancellot.eth <heronlancellot@id.uff.br>
2024-06-14 11:36:14 -0300

    feat: enable the button when the recipient is the default value or the correct recipient

--- components/02-molecules/EtherFieldAddition.tsx
+++ components/02-molecules/EtherFieldAddition.tsx
@@ -2,6 +2,7 @@ import { EtherAmountSelectionModal } from "@/components/02-molecules";
 import { EditionIcon, PlusIconSmall } from "@/components/01-atoms/";
 import { SwapContext } from "@/lib/client/contexts";
 import { ForWhom } from "@/components/03-organisms";
+import { isInRange } from "@/lib/client/utils";
 import { useState, useContext, useEffect } from "react";
 import { formatEther } from "viem";
 import { useNetwork } from "wagmi";
@@ -9,26 +10,37 @@ import { useNetwork } from "wagmi";
 export const EtherFieldAddition = ({ variant }: { variant: ForWhom }) => {
   const [open, setIsOpen] = useState(false);
   const [isMounted, setIsMounted] = useState(false);
-  const { etherValue } = useContext(SwapContext);
+  const { etherValue, etherRecipient } = useContext(SwapContext);
   const { chain } = useNetwork();
-
-  // if (variant === ForWhom.Yours && !etherValue) {
-  //   return <></>;
-  // }
 
   useEffect(() => {
     setIsMounted(true);
   }, []);
 
+  console.log("etherRecipient", etherRecipient);
   return (
     <div className="flex gap-2">
-      <p className="flex items-center p-small-variant-black-3 dark:p-small-variant-light-2">
-        {formatEther(etherValue)}
-      </p>
-      {/* Only render the chain symbol after the component has mounted */}
-      <p>{isMounted && chain ? chain.nativeCurrency.symbol : ""}</p>
-
-      {etherValue === 0n ? (
+      <div className="flex gap-1">
+        {variant === ForWhom.Yours &&
+        (etherRecipient === 0 || etherRecipient === 256) ? (
+          <>
+            <p className="flex items-center p-small-variant-black-3 dark:p-small-variant-light-2">
+              {formatEther(etherValue)}
+            </p>
+            {/* Only render the chain symbol after the component has mounted */}
+            <p>{isMounted && chain ? chain.nativeCurrency.symbol : ""}</p>
+          </>
+        ) : variant === ForWhom.Their && isInRange(etherRecipient, 1, 256) ? (
+          <>
+            <p className="flex items-center p-small-variant-black-3 dark:p-small-variant-light-2">
+              {formatEther(etherValue)}
+            </p>
+            {/* Only render the chain symbol after the component has mounted */}
+            <p>{isMounted && chain ? chain.nativeCurrency.symbol : ""}</p>
+          </>
+        ) : null}
+      </div>
+      {etherValue === 0n || isInRange(etherRecipient, 0, 256) ? (
         <>
           <button
             className="w-6 h-6 rounded-full bg-[#FFFFFF1A] flex justify-center items-center"
@@ -46,7 +58,8 @@ export const EtherFieldAddition = ({ variant }: { variant: ForWhom }) => {
             variant={variant}
           />
         </>
-      ) : (
+      ) : variant === ForWhom.Yours &&
+        (etherRecipient === 0 || etherRecipient === 256) ? (
         <>
           <button
             className="w-6 h-6 rounded-full bg-[#FFFFFF1A] flex justify-center items-center"
@@ -64,7 +77,25 @@ export const EtherFieldAddition = ({ variant }: { variant: ForWhom }) => {
             variant={variant}
           />
         </>
-      )}
+      ) : variant === ForWhom.Their && isInRange(etherRecipient, 1, 256) ? (
+        <>
+          <button
+            className="w-6 h-6 rounded-full bg-[#FFFFFF1A] flex justify-center items-center"
+            onClick={() => {
+              setIsOpen(!open);
+            }}
+          >
+            <EditionIcon />
+          </button>
+          <EtherAmountSelectionModal
+            open={open}
+            onClose={() => {
+              setIsOpen(false);
+            }}
+            variant={variant}
+          />
+        </>
+      ) : null}
     </div>
   );
 };
