a762568ce6a47d64ef383cd93f7a0f8091a39afc
heronlancellot.eth <heronlancellot@id.uff.br>
2024-06-24 11:29:04 -0300

    feat: add token-3d-modal with link to etherscan

--- components/01-atoms/Token3DModal.tsx
+++ components/01-atoms/Token3DModal.tsx
@@ -1,7 +1,6 @@
 import { TokenCard, TokenCardActionType } from "../02-molecules";
+import { BlockExplorerExternalLinkButton } from "@/components/01-atoms";
 import { ERC721, EthereumAddress, Token, TokenType } from "@/lib/shared/types";
-import { collapseAddress } from "@/lib/client/utils";
-import React from "react";
 import cc from "classcat";
 import { Atropos } from "atropos/react";
 
@@ -29,14 +28,16 @@ export const Token3DModal = ({
       )}
       <div
         className={cc([
-          "fixed z-50 flex justify-center items-center transition-transform duration-300 ease-in-out p-6",
+          "fixed z-50 flex justify-center items-center transition-transform duration-300 ease-in-out p-6 ",
           "rounded-[20px] border dark:bg-[#212322] dark:border-[#353836] dark:shadow-sidebarDark bg-[#F6F6F6] border-[#F0EEEE] shadow-sidebarLight",
-          isOpen ? "opacity-100 translate-y-0" : "opacity-0 translate-y-full",
-          "top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2",
+          isOpen ? "opacity-100 scale-100 " : "opacity-0 scale-75",
+          isOpen
+            ? "top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[800px] h-[500px]"
+            : "translate-y-full",
         ])}
       >
-        <div className="flex w-full h-full gap-2">
-          <div className="flex w-full h-full">
+        <div className="flex flex-row w-full h-full gap-2">
+          <div className="flex w-full h-full justify-center">
             <Atropos>
               <TokenCard
                 tokenData={token}
@@ -48,15 +49,21 @@ export const Token3DModal = ({
               />
             </Atropos>
           </div>
-          <div className="flex flex-col">
-            <p>
+          <div className="flex h-full flex-col mt-4">
+            <p className="text-lg font-bold">
               {token.name} (
               {token.tokenType === TokenType.ERC721 &&
                 (token as ERC721).contractMetadata?.symbol}
               )
             </p>
-            <p>#{token.id}</p>
-            <p>{token.contract && collapseAddress(token.contract)}</p>
+            <p className="text-md">#{token.id}</p>
+            <p className="text-md">
+              {token.contract && (
+                <BlockExplorerExternalLinkButton
+                  address={new EthereumAddress(token.contract)}
+                />
+              )}
+            </p>
           </div>
         </div>
       </div>
