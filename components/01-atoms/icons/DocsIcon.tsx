dee2f45bb09625ef24e804d88e73751b4605fc16
heronlancellot.eth <heronlancellot@id.uff.br>
2024-07-04 12:21:17 -0300

    feat: add github & doc icon

---
+++ components/01-atoms/icons/DocsIcon.tsx
@@ -0,0 +1,34 @@
+import React, { SVGProps } from "react";
+
+export const DocsIcon = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      viewBox="0,0,256,256"
+      width="30px"
+      height="30px"
+    >
+      <g
+        fill="currentColor"
+        fill-rule="nonzero"
+        stroke="none"
+        stroke-width="1"
+        stroke-linecap="butt"
+        stroke-linejoin="miter"
+        stroke-miterlimit="10"
+        stroke-dasharray=""
+        stroke-dashoffset="0"
+        font-family="none"
+        font-weight="none"
+        font-size="none"
+        text-anchor="none"
+      >
+        <g transform="scale(10.66667,10.66667)">
+          <path d="M13.172,2h-7.172c-1.1,0 -2,0.9 -2,2v16c0,1.1 0.9,2 2,2h12c1.1,0 2,-0.9 2,-2v-11.172c0,-0.53 -0.211,-1.039 -0.586,-1.414l-4.828,-4.828c-0.375,-0.375 -0.884,-0.586 -1.414,-0.586zM12.5,19h-4c-0.276,0 -0.5,-0.224 -0.5,-0.5v-1c0,-0.276 0.224,-0.5 0.5,-0.5h4c0.276,0 0.5,0.224 0.5,0.5v1c0,0.276 -0.224,0.5 -0.5,0.5zM15.5,16h-7c-0.276,0 -0.5,-0.224 -0.5,-0.5v-1c0,-0.276 0.224,-0.5 0.5,-0.5h7c0.276,0 0.5,0.224 0.5,0.5v1c0,0.276 -0.224,0.5 -0.5,0.5zM15.5,13h-7c-0.276,0 -0.5,-0.224 -0.5,-0.5v-1c0,-0.276 0.224,-0.5 0.5,-0.5h7c0.276,0 0.5,0.224 0.5,0.5v1c0,0.276 -0.224,0.5 -0.5,0.5zM13,9v-5.5l5.5,5.5z"></path>
+        </g>
+      </g>
+    </svg>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,25 @@
-import { BannerMintTokens } from "@/components/01-atoms";
+import { BannerMintTokens, DocsIcon, GithubLogo } from "@/components/01-atoms";
 import { Layout, SwapSection } from "@/components/04-templates";
+import Link from "next/link";
 
 export default function IndexPage() {
   return (
     <Layout>
       <div className="w-screen h-screen flex flex-col justify-center items-center">
         <SwapSection />
-        <div className="flex xl:flex-row flex-col lg:justify-center ">
+        <div className="flex xl:flex-row flex-col lg:justify-center gap-2 ">
           <BannerMintTokens />
+          <div className="flex items-center gap-1">
+            <Link href={"https://blockful.gitbook.io/swaplace"} target="_blank">
+              <DocsIcon className="dark:text-[#DDF23D] text-[#A3A9A5]" />
+            </Link>
+            <Link
+              href={"https://github.com/blockful-io/swaplace-contracts"}
+              target="_blank"
+            >
+              <GithubLogo className="dark:text-[#DDF23D] text-[#A3A9A5]" />
+            </Link>
+          </div>
         </div>
       </div>
     </Layout>
