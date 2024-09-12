4a5f76eb41b4ba9087ec981bb1c680c6f8e4c2dc
heronlancellot.eth <heronlancellot@id.uff.br>
2024-09-12 09:30:15 -0300

    Update components/04-templates/SwapSection.tsx
    Co-authored-by: caco.eth <49823133+FrancoAguzzi@users.noreply.github.com>

--- components/04-templates/SwapSection.tsx
+++ components/04-templates/SwapSection.tsx
@@ -9,7 +9,7 @@ export const SwapSection = () => {
   return (
     <div className="max-w-[1280px] max-h-[720px] w-full flex xl:flex-row flex-col lg:justify-center h-full">
       <TheHeader />
-      <section className="flex items-center xl:px-[60px] pt-[32px] xl:flex-row flex-col">
+      <section className="flex items-center xl:px-[60px] pt-8 xl:flex-row flex-col">
         <div className="flex lg:flex-row flex-col xl:h-[656px] xl:w-[1098px] lg:gap-[20px] w-[95%] gap-2 xl:pt-0">
           <div className="flex flex-col lg:w-[600px] gap-8">
             <SwapConnection />
