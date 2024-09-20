c3a6bc19335741d8d6ab1926757b0b4479689bc7
heronlancellot.eth <heronlancellot@id.uff.br>
2024-09-20 10:59:09 -0300

    Update components/01-atoms/SearchBar.tsx
    Co-authored-by: caco.eth <49823133+FrancoAguzzi@users.noreply.github.com>

--- components/01-atoms/SearchBar.tsx
+++ components/01-atoms/SearchBar.tsx
@@ -15,8 +15,7 @@ import Web3 from "web3";
 import toast from "react-hot-toast";
 
 /**
- * These sections are currently commented out because the `ens-avatar-searched-address` has issues fetching the correct address.
- * Since ENS related feature is not working properly, we are commenting those sections.
+ * These sections are currently commented out because we are facing issues fetching the correct address ENS name Ethereum address. Since ENS related feature is not working properly, we are commenting those sections.
  */
 
 export const SearchBar = () => {
